71d75d94fdbc14673bee092bdf5835373fc86dc3
Odi-gcapela <gcapela@siue.edu>
2025-11-15 22:40:37 -0600

    Win_Chest
    Was able to get a win screen, however only if the head touches the chest

---
+++ Assets/Chest_Win.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class Chest_Win : MonoBehaviour{
+
+    [SerializeField] 
+    private GameObject WinScreen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+    
+        if (other.CompareTag("Player"))
+        {
+            WinScreen.SetActive(true); 
+            Time.timeScale = 0f;       
+        }
+    }
+}
